fc6ea73a1d0b3613d51e2c85f13eff813f6cd41a
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-09-18 03:48:26 +0200

    Complete the comparePasswords method

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -20,7 +20,7 @@ async function generatePassword(plainTextPassword: string): Promise<string> {
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     // Use Bcrypt to Compare your password to your Salted Hashed Password
-    return false
+    return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
