b207296792058fb6e0737397a6acd3a65544a036
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-09-18 05:36:57 +0200

    Complete the JWT stubs

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,17 @@
 export const config = {
-  "dev": {
+  "postgres": {
     "username": "",
     "password": "",
     "database": "",
     "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
     "dialect": "postgres"
+  },
+  "aws": {
+    "aws_region": "eu-central-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "my-udagram"
+  },
+  "jwt": {
+    "secret": ""
   }
 }
