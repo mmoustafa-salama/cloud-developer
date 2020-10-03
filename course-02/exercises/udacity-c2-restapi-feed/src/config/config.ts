3b6d1ad53395449f2fec283ce040106c8ba955dc
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-03 17:29:05 +0200

    Divide the Backend into two microservices feed & user

---
+++ course-02/exercises/udacity-c2-restapi-feed/src/config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  "postgres": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": ""
+  },
+  "aws": {
+    "aws_region": "",
+    "aws_profile": "",
+    "aws_media_bucket": ""
+  },
+  "jwt": {
+    "secret": ""
+  }
+}
