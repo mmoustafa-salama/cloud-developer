8f70c7a761bb2009e5b4105d2ec4831329645e4a
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-30 19:15:46 +0200

    Implement getting TODO items

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -2,6 +2,39 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import { getUserId } from '../utils'
+
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  // Get all TODO items for a current user
+  const userId = getUserId(event);
+
+  const todos = await getUserTodos(userId);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todos
+    })
+  }
+}
+
+async function getUserTodos(userId: string) {
+  const result = await docClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return result.Items
 }
