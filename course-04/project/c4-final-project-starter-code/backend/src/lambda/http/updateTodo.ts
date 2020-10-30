3a0243f05b4392c042825264e55fb0f03c53b820
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-30 19:14:59 +0200

    Implement update an existing TODO item

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -4,10 +4,69 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
+import * as AWS from 'aws-sdk'
+import { TodoItem } from '../../models/TodoItem'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const todoIndexName = process.env.TODO_INDEX_NAME
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  // Update a TODO item with the provided id using values in the "updatedTodo" object
+  const todoItem = await updateTodo(todoId, updatedTodo);
+  if (!todoItem) {
+    return {
+      statusCode: 404,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: `No todo with the Id '${todoId}' exist`
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      item: todoItem,
+    })
+  }
+}
+
+async function updateTodo(todoId: string, updatedTodo: UpdateTodoRequest) {
+
+  const todoItem = await getTodoById(todoId)
+  if (todoItem) {
+    todoItem.name = updatedTodo.name;
+    todoItem.dueDate = updatedTodo.dueDate;
+    todoItem.done = updatedTodo.done;
+
+    await docClient.put({
+      TableName: todosTable,
+      Item: todoItem
+    }).promise()
+  }
+  return todoItem;
+}
+
+async function getTodoById(todoId: string): Promise<TodoItem> {
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: todoIndexName,
+    KeyConditionExpression: 'todoId = :todoId',
+    ExpressionAttributeValues: {
+      ':todoId': todoId
+    }
+  }).promise()
+
+  if (result.Count !== 0) {
+    return result.Items[0] as TodoItem
+  }
+
+  return undefined;
 }
