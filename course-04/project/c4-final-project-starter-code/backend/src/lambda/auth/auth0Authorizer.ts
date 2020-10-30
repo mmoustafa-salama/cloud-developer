d9172e6475c870d8fdef83d28cfb42b6957c38d6
Mahmoud Moustafa <m.salama@edraksoftware.com>
2020-10-30 19:21:00 +0200

    Implement custom authorizer to validate Auth0 tokens

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,20 +3,25 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import * as AWS from 'aws-sdk'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// const jwksUrl = 'https://cloud-dev.eu.auth0.com/.well-known/jwks.json'
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+const secretClient = new AWS.SecretsManager();
+
+let cachedSecret;
+
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -58,10 +63,16 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
+  if (!jwt) {
+  }
+
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+
+  const secretObject = await getSecret();
+  const auth0Secret = secretObject[secretField];
+  return verify(token, auth0Secret) as JwtPayload;
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +85,16 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  const data = await secretClient.getSecretValue({
+    SecretId: secretId
+  }).promise()
+
+  cachedSecret = JSON.parse(data.SecretString);
+
+  return cachedSecret;
 }
